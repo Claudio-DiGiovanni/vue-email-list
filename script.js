572a4963ee3b2e7b925b7a9f5136d7cd8c8f3065
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2022-11-22 14:46:07 +0100

    consegna

---
+++ script.js
@@ -0,0 +1,21 @@
+/* generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
+new Vue ({
+    el: "#app",
+    data: {
+        arrEmail: []
+    },
+    computed: {
+        getRandomEmails() {
+            this.arrEmail = [];
+			for (let i = 0; i < 10; i++) {
+				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+				.then(axiosResponse => {
+					this.arrEmail.push(axiosResponse.data.response);
+				})
+			}
+        }
+    }
+}) 
